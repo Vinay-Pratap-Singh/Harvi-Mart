a95b57e910ba443c1f19993dff70e79554fa585d
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-12-04 13:53:19 +0530

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav>
-      {/* adding the logo here */}
-      <NavLink to={"/"}>Electro Mart</NavLink>
+  const showAndHideMenu = (event) => {
+    const menu = event.target.nextElementSibling;
+    if (menu.style.display === "none") menu.style.display = "flex";
+    else menu.style.display = "none";
+  }
 
-      <div>
+  return (
+    <nav className="flex flex-row-reverse sm:flex-row sm:items-center justify-around py-4 sticky top-0">
+      {/* adding the logo here */}
+      <NavLink className="font-bold text-xl" to={"/"}>Electro Mart</NavLink>
+
+      <div className="flex flex-col gap-2">
         {/* adding the menu bar for mobile screen */}
-        <i class="fa-solid fa-bars"></i>
+        <i onClick={showAndHideMenu} className="fa-solid fa-bars sm:hidden"></i>
         {/* creating the menu item */}
-        <div>
+        <div className="hidden sm:flex flex-col sm:flex-row gap-2 sm:gap-6">
           <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/about"}>About</NavLink>
           <NavLink to={"./products"}>Products</NavLink>
