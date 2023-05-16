39cfd8c9009beaedbaf5b7cf4f02fc5a5c0fd3dd
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-05-16 10:59:19 +0530

    setup redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
-    <App />
+    <Provider store={store}>
+      <ColorModeScript />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
