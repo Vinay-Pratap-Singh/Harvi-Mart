00d2e06b49a2899bcc21dba9b270990fb04ec300
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-06-12 23:35:08 +0530

    feat: dynamic product data on product page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,29 +1,41 @@
-import { Box, Button, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import productImage from "../assets/CategoryImages/shoes.png";
 import { AiFillHeart } from "react-icons/ai";
 
 const ProductCard = ({ product }: any) => {
-  console.log(product);
-
-  const productDetail = {
-    title: "Stylish Shoes",
-    description: "Best sports shoe for a stylish look and confortable feel",
-    originalPrice: 100,
-    productImage: productImage,
-    category: "shoes",
-  };
   return (
     <VStack w={60} p={2} rounded={"md"} shadow={"md"} pos={"relative"}>
       <Box pos={"absolute"} right={2} cursor={"pointer"}>
         <AiFillHeart fontSize={"25px"} />
       </Box>
-      <Image h={40} src={productDetail.productImage} />
-      <Heading fontSize={"xl"}>{productDetail.title}</Heading>
-      <Text fontWeight={"bold"}>
-        {productDetail.originalPrice} &#x20b9; only
-      </Text>
+      <Image h={40} src={product?.images[0]?.image?.secure_url} />
+      <Heading fontSize={"md"} fontWeight={"semibold"}>
+        {product?.title}
+      </Heading>
+      {product?.discountedPrice ? (
+        <HStack>
+          <Text fontWeight={"semibold"} fontSize={"md"}>
+            &#x20b9;{product?.discountedPrice}
+          </Text>
+          <Text fontSize={"sm"} fontWeight={"semibold"}>
+            <s>&#x20b9;{product?.originalPrice}</s>
+          </Text>
+        </HStack>
+      ) : (
+        <Text fontSize={"sm"} fontWeight={"semibold"}>
+          &#x20b9;{product?.originalPrice}
+        </Text>
+      )}
       <Button w={"full"} colorScheme="orange">
-        Product Details
+        More Details
       </Button>{" "}
     </VStack>
   );
