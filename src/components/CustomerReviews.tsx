694345a46d11a483a393acd76fb43e680a3f245e
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-06-16 14:55:26 +0530

    feat: added dynamic reviews on the product description page

--- src/components/CustomerReviews.tsx
+++ src/components/CustomerReviews.tsx
@@ -1,13 +1,9 @@
 import { Flex, Heading, IconButton, Text, VStack } from "@chakra-ui/react";
 import { AiFillStar } from "react-icons/ai";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
 import { useEffect } from "react";
-import {
-  getAllReviews,
-  getIndividualProductReview,
-  getIndividualReview,
-} from "../redux/reviewSlice";
+import { getIndividualProductReview } from "../redux/reviewSlice";
 
 interface Iprop {
   productID: string;
@@ -16,46 +12,54 @@ interface Iprop {
 const CustomerReviews: React.FC<Iprop> = ({ productID }) => {
   const dispatch = useDispatch<AppDispatch>();
 
-  const reviews = [
-    {
-      title: "Good Product",
-      rating: 4,
-      review: "This is my first purchase and i am very happy with it",
-    },
-    {
-      title: "Bad Product",
-      rating: 1,
-      review: "This is my first purchase and i am very sad with this  product",
-    },
-  ];
+  const { reviews } = useSelector((state: RootState) => state.review);
 
   // getting the product review
   useEffect(() => {
     (async () => {
       await dispatch(getIndividualProductReview(productID));
-      // await dispatch(getAllReviews());
     })();
   }, []);
 
   return (
     <VStack w={"full"} alignItems={"flex-start"}>
-      {reviews.map((review, index) => {
+      {reviews.map((review: any) => {
+        const formattedDate = new Date(review?.createdAt).toLocaleString(
+          "en-US",
+          {
+            dateStyle: "medium",
+          }
+        );
+        const formattedTime = new Date(review?.createdAt).toLocaleString(
+          "en-US",
+          {
+            timeStyle: "short",
+          }
+        );
         return (
-          <VStack key={index} alignItems={"flex-start"} w={"full"} spacing={0}>
-            <Heading fontSize={"md"} fontWeight={"bold"}>
+          <VStack
+            key={review?._id}
+            alignItems={"flex-start"}
+            w={"full"}
+            spacing={0}
+          >
+            <Heading fontSize={"xl"} fontWeight={"bold"}>
               {review.title}
             </Heading>
+            <Text fontSize={"sm"} color={"gray.600"}>
+              Reviewed on {formattedDate} {formattedTime}
+            </Text>
             <Text fontWeight={"medium"}>{review.review}</Text>
             <Flex>
               {[...Array(5)].map((_, i) => (
                 <IconButton
                   key={i}
-                  icon={<AiFillStar fontSize={28} />}
+                  icon={<AiFillStar fontSize={20} />}
                   variant={"ghost"}
                   _hover={{ bg: "none" }}
                   color={review.rating >= i + 1 ? "yellow.400" : "gray.200"}
                   aria-label={`Star ${i + 1}`}
-                  size="sm"
+                  size="xs"
                 />
               ))}
             </Flex>
