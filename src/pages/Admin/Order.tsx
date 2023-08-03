c56bf93465cd61ebe48f7bff8fd3e079f876f804
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-08-03 22:04:41 +0530

    feat: implemented the orders page to display order

--- src/pages/Admin/Order.tsx
+++ src/pages/Admin/Order.tsx
@@ -4,7 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
 import { getAllOrders } from "../../redux/orderSlice";
 import { useEffect } from "react";
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
+import { IordersData } from "../../helper/interfaces";
+import { MdOutlineDescription } from "react-icons/md";
 
 const Order = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,13 +41,91 @@ const Order = () => {
         />
       </Helmet>
 
-      <VStack w={"full"} pl={60} pt={5}>
+      <VStack w={"full"} pl={60} pt={5} gap={10}>
         <Heading fontSize={"3xl"}>
           Welcome to the{" "}
           <Text as={"span"} color={"primaryColor"}>
             Order Page
           </Text>{" "}
         </Heading>
+
+        {/* for displaying the product table */}
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th isNumeric textAlign={"center"}>
+                  S. No.
+                </Th>
+                <Th>Ordered By</Th>
+                <Th>Email</Th>
+                <Th>Phone No.</Th>
+                <Th>Purchase Amount</Th>
+                <Th>Order Status</Th>
+                <Th>Payment Method</Th>
+                <Th>Payment Status</Th>
+                <Th>Actions</Th>
+              </Tr>
+            </Thead>
+
+            {/* adding the table body */}
+            <Tbody fontSize={"14.5px"} fontWeight={"semibold"}>
+              {orders.length === 0 ? (
+                <Tr>
+                  <Td colSpan={9}>Oops! There is no products</Td>
+                </Tr>
+              ) : (
+                orders.map((order: IordersData, index) => {
+                  return (
+                    <Tr key={order._id}>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {index < 10 ? `0${index + 1}` : index + 1}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {order?.user?.fullName}
+                      </Td>
+                      <Td p="1" verticalAlign={"text-top"}>
+                        {order?.user?.email}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {order?.phoneNumber}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        &#x20b9;{order?.total}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {order?.orderStatus}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {order?.paymentMethod}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        {order?.orderStatus}
+                      </Td>
+                      <Td p="1" textAlign={"center"} verticalAlign={"text-top"}>
+                        <Tooltip
+                          hasArrow
+                          label="More order details"
+                          color={"orange.500"}
+                          bgColor={"white"}
+                          placement={"top"}
+                        >
+                          <Button
+                            p="0"
+                            _hover={{ color: "#e06464" }}
+                            fontSize={"xl"}
+                          >
+                            <MdOutlineDescription />
+                          </Button>
+                        </Tooltip>
+                      </Td>
+                    </Tr>
+                  );
+                })
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </VStack>
     </Layout>
   );
