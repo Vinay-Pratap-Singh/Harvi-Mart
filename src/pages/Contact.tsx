caa5914d44e7937689470b3705a0082fe97ffa5f
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-09-26 10:14:54 +0530

    fix: blank data on form submission

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,18 +1,25 @@
 import {
   Box,
   Button,
+  FormControl,
+  FormLabel,
   HStack,
   Heading,
   Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
   Link,
   Text,
+  Textarea,
   VStack,
 } from "@chakra-ui/react";
 import myImage from "../assets/contactPage.jpg";
 import { Link as RouterLink } from "react-router-dom";
-import { BiArrowBack } from "react-icons/bi";
+import { BiArrowBack, BiUser } from "react-icons/bi";
 import { Helmet } from "react-helmet";
 import Layout from "./Layout/Layout";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Contact = () => {
   return (
@@ -33,7 +40,7 @@ const Contact = () => {
         alignItems={"center"}
         justifyContent={"center"}
       >
-        <form name="contact" method="post">
+        <form name="contact" method="post" data-netlify="true">
           {/* for netlify bot */}
           <input type="hidden" name="form-name" value="contact" />
 
@@ -66,7 +73,7 @@ const Contact = () => {
               </Heading>
 
               {/* for name */}
-              <label className="contact-form-label">
+              {/* <label className="contact-form-label">
                 Your Full Name
                 <input
                   className="contact-form-input"
@@ -75,10 +82,10 @@ const Contact = () => {
                   required
                   minLength={5}
                 />
-              </label>
+              </label> */}
 
               {/* for email */}
-              <label className="contact-form-label">
+              {/* <label className="contact-form-label">
                 Your Email
                 <input
                   className="contact-form-input"
@@ -87,10 +94,10 @@ const Contact = () => {
                   required
                   pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
                 />
-              </label>
+              </label> */}
 
               {/* for message */}
-              <label className="contact-form-label">
+              {/* <label className="contact-form-label">
                 Message
                 <textarea
                   className="contact-form-textarea"
@@ -98,7 +105,70 @@ const Contact = () => {
                   required
                   minLength={20}
                 />
-              </label>
+              </label> */}
+
+              {/* for name */}
+              <FormControl>
+                <FormLabel fontSize={"sm"} htmlFor="name">
+                  Your Full Name
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    fontSize={"xl"}
+                    color={"orange.500"}
+                    children={<BiUser />}
+                  />
+                  <Input
+                    type="text"
+                    focusBorderColor="primaryColor"
+                    placeholder="Vinay Pratap Singh Harvi"
+                    required
+                    minLength={3}
+                    name="name"
+                    id="name"
+                  />
+                </InputGroup>
+              </FormControl>
+
+              {/* for email */}
+              <FormControl>
+                <FormLabel fontSize={"sm"} htmlFor="email">
+                  Your Email
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    fontSize={"xl"}
+                    color={"orange.500"}
+                    children={<HiOutlineMail />}
+                  />
+                  <Input
+                    type="email"
+                    focusBorderColor="primaryColor"
+                    placeholder="test@gmail.com"
+                    required
+                    pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
+                    name="email"
+                    id="email"
+                  />
+                </InputGroup>
+              </FormControl>
+
+              {/* for message */}
+              <FormControl pos={"relative"}>
+                <FormLabel fontSize={"sm"} htmlFor="message">
+                  Message
+                </FormLabel>
+                <Textarea
+                  height={24}
+                  focusBorderColor="primaryColor"
+                  resize={"none"}
+                  placeholder="Enter your message..."
+                  required
+                  minLength={20}
+                  name="message"
+                  id="message"
+                />
+              </FormControl>
 
               {/* submit button */}
               <Button type="submit" w={"full"} colorScheme="orange">
